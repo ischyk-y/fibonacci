7f4e6d3d87c332c55a1f0b30f260ed1f392c96e8
Yaroslav Ischyk <ischyk.y@gmail.com>
2024-09-15 19:06:33 +0300

    fibonacci

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from dependecies import get_text
+def fibonacci(n):
+    if n <= 0:
+        return "Число має бути більше нуля"
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n):
+            a, b = b, a + b
+        return b
 
 
-def main():
-    print(get_text("Test"))
+n = int(input("Введіть номер числа Фібоначчі: "))
+print(f"Число Фібоначчі під номером {n} дорівнює {fibonacci(n)}")
 
-
-if __name__ == '__main__':
-    main()
